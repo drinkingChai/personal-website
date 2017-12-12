bcda30497e444e638c62e1d9a770bce513d4d405
Wasif Zaman <teatocode@gmail.com>
2017-12-12 00:23:16 -0500

    contact test

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
 const Contact = ({ email }) => {
   // business card
   return (
     <div className='contact-container'>
-      { email }
+      <div className='emails'>
+        <h3>Contact</h3>
+        <p>{ email }</p>
+        <CopyToClipboard text={ email }><span>Click to copy my email address</span></CopyToClipboard>
+        <a href={ `mailto:${email}` }>Open your email app</a>
+      </div>
     </div>
   )
 }
