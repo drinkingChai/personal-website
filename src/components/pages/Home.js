aeccaa4eedd89db00e29bcae9eaf8eca24750638
Wasif Zaman <teatocode@gmail.com>
2017-12-13 23:59:56 -0500

    added logo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,8 +11,8 @@ const Home = ({ me, projects, className }) => {
   return (
     <div className={ `home ${className || ''}` }>
       <div className='me'>
-        <div className='click-the-bat'>Click the bat!</div>
-        <LinkTransition to='/about' className='batman'></LinkTransition>
+        <div className='click-the-bat'>Click the Logo!</div>
+        <LinkTransition to='/about' className='logo'></LinkTransition>
         <h1>Wasif Zaman</h1>
         <h2>Full Stack Developer</h2>
       </div>
