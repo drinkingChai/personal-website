75b06bbd69686487aa5d03157116ca6734e35cd0
Wasif Zaman <teatocode@gmail.com>
2017-12-14 15:15:40 -0500

    changed it back to smart component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,53 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link as ScrollLink, Element } from 'react-scroll'
 
 import IconLink from '../iconLinkGenerator'
 
-const Projects = ({ projects }) => {
-  window.scrollTo(0, 0)
+class Projects extends Component {
+  state = { activeProject: 0 }
 
-  return (
-    <div className='projects-container'>
-      <div className='bubble-scrollbar'>
-      {
-        projects.length && projects.map((project, i) => 
-          <ScrollLink
-            to={ `project-${i}` }
-            smooth={ true }
-            offset={ -80 }
-            duration={ 300 }
-            spy={ true }
-            activeClass={ 'active' }
-            className='bubble'
-            key={ i }
-            onClick={ () => this.scrollTo(i) }></ScrollLink>
-        )
-      }
-      </div>
-      <h3 className='page-title'>Projects</h3>
-      <div className='projects-card-container'>
+  componentDidMount = () => {
+    window.scrollTo(0, 0)
+  }
+
+  scrollTo = i => {
+    this.setState({ activeProject: i })
+  }
+  
+  render = () => {
+    const { projects } = this.props
+
+    return (
+      <div className='projects-container'>
+        <div className='bubble-scrollbar'>
         {
-          projects.length && projects.map((project, i) =>
-            <Element className='project-card element' key={ i } name={ `project-${i}` }>
-              { project.thumbnail ?
-                <div className='thumbnail-container'>
-                  <img src={ project.thumbnail } />
-                </div> : null }
-              <div className='project-info'>
-                <h4>{ project.name }</h4>
-                <p>{ project.description }</p>
-                <div className='project-links'>
-                  <a href={ project.url } target='_blank'>Demo</a>
-                  <a href={ project.github } target='_blank'>Code</a>
-                </div>
-              </div>
-            </Element>
+          projects.length && projects.map((project, i) => 
+            <ScrollLink
+              to={ `project-${i}` }
+              smooth={ true }
+              offset={ -80 }
+              duration={ 300 }
+              spy={ true }
+              activeClass={ 'active' }
+              className='bubble'
+              key={ i }
+              onClick={ () => this.scrollTo(i) }></ScrollLink>
           )
         }
+        </div>
+        <h3 className='page-title'>Projects</h3>
+        <div className='projects-card-container'>
+          {
+            projects.length && projects.map((project, i) =>
+              <Element className='project-card element' key={ i } name={ `project-${i}` }>
+                { project.thumbnail ?
+                  <div className='thumbnail-container'>
+                    <img src={ project.thumbnail } />
+                  </div> : null }
+                <div className='project-info'>
+                  <h4>{ project.name }</h4>
+                  <p>{ project.description }</p>
+                  <div className='project-links'>
+                    <a href={ project.url } target='_blank'>Demo</a>
+                    <a href={ project.github } target='_blank'>Code</a>
+                  </div>
+                </div>
+              </Element>
+            )
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapState = ({ projects }) => ({ projects })
