615018997801f6d51ee7acf4e0dd55e98f868307
Wasif Zaman <teatocode@gmail.com>
2017-12-14 16:50:41 -0500

    up to 200 slidedowns

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,23 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Transition = ({ transition }) => {
+  const directions = [ 'left-angle', 'right-angle', 'left', 'right' ],
+    direction = directions[Math.floor(Math.random()*4 + 0)],
+    numSlides = Math.ceil(window.innerWidth / 30),
+    slides = []
+
+  for(let i = 0; i < numSlides; i++) {
+    slides.push(<div className='slide' key={ i }></div>)
+  }
+
   return (
     <span>
     { transition ?
-      <div className='transition slides'>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
-        <div className='slide'></div>
+      <div className={ `transition slides` }>
+      { slides.map(slide => slide) }
       </div> : null
     }
     </span>
